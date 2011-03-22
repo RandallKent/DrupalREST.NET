9a593fbea703bd378e29f7f02807a8f7dcdb1616
Randall Kent <rkent@sevaa.com>
2011-03-22 18:19:22 -0400

    basic get and post methods as well as the start of the login method

---
+++ DrupalREST.NET/DrupalREST.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Web;
+
+
+namespace DrupalREST
+{
+    public class DrupalRESTInterface
+    {
+        public string DrupalEndpoint { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Session { get; set; }
+
+        public string Login()
+        {
+
+            Dictionary<string, string> postables = new Dictionary<string, string>();
+
+            postables.Add("username", this.Username);
+            postables.Add("password", this.Password);
+
+            var result = HttpPost(this.DrupalEndpoint + "user/login/", postables);
+
+            //TOOD: Handle result to extract session informatino which needs to be stored for future authenticated requests
+            return result;
+        }
+
+        static string HttpGet(string url)
+        {
+            // Setup the Request
+            HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+            string result = null;
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        static string HttpPost(string url, Dictionary<string, string> paramz)
+        {
+
+            // Setup the Request
+            HttpWebRequest request = WebRequest.Create(new Uri(url)) as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            StringBuilder query = new StringBuilder();
+            foreach (KeyValuePair<string, string> param in paramz)
+            {
+                query.AppendFormat("{0}={1}&", param.Key, HttpUtility.UrlEncode(param.Value));
+            }
+
+            // Encode the parameters as form data:
+            byte[] formData = UTF8Encoding.UTF8.GetBytes(query.ToString());
+
+            request.ContentLength = formData.Length;
+
+            // Send the request:
+            using (Stream post = request.GetRequestStream())
+            {
+                post.Write(formData, 0, formData.Length);
+            }
+
+            // Pick up the response:
+            string result = null;
+            using (HttpWebResponse resp = request.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+    }
+}
